3fdb135c1f3cd4e1072fdbd56ff23f494cc3bb53
unknown <kevwenner@gmail.com>
2022-12-22 21:23:06 -0500

    cleaned up randomization and validation

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # PasswordGenerator
+
+##Description
+
+-This project was made to generate unique strings for various uses
+-Pactice in nested arrays, loops,  and math function
+-having to come up with unique character strings on your own. Just press a button
+
+##How to Use
+
+-press the generate Password button to begin generating a password
+-specify character length of password (must be 8 or more and 128 or less characters)
+-choose types of characters within prompts to use (must select at least one character type)
+-password will be generated and placed within the text field
+
+##license
+
+Refer to GNU license within the file
--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; //9
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", ",", "<", ".", ">", "/", "?", ";", ":", "'"]; //21
 var pwOptions = [lowerCase, upperCase, numbers, symbols];
+var arrayHolder = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -18,8 +19,12 @@ function generatePassword(){
   var passwordPlaceHolder = [];
   // makes prompts and adds responses to passwordPlaceHolder
   function makeArray(){
+    passwordPlaceHolder = [];
+    arrayHolder.forEach(element => {
+      pwOptions.splice(0, 0, arrayHolder);
+    });
 
-    var pwLength = prompt("How many characters long would you like your password? Must be no shorter than 8 and no longer than 128.")
+    var pwLength = prompt("How many characters long would you like your password?")
 
     passwordPlaceHolder.push(pwLength);
 
@@ -27,60 +32,86 @@ function generatePassword(){
 
     passwordPlaceHolder.push(pwLower);
 
+    // if no move out of array to arrayHolder
+    if (pwLower == true) {
+      arrayHolder.splice(0, 0, pwOptions[0]);
+      // console.log(arrayHolder);
+    }
+
     var pwUpper = confirm("Do you want to use upper case characters?");
 
     passwordPlaceHolder.push(pwUpper);
+
+    if (pwUpper == true) {
+      arrayHolder.splice(0, 0, pwOptions[1]);
+      // console.log(arrayHolder);
+    }
+    // if no move out of array to arrayHolder
 
     var pwNumeric = confirm("Do you want to use numeric characters?");
 
     passwordPlaceHolder.push(pwNumeric);
 
+    // if no move out of array to arrayHolder
+    if (pwNumeric == true) {
+      arrayHolder.splice(0, 0, pwOptions[2]);
+      // console.log(arrayHolder);
+    }
+
     var pwSpecial = confirm("Do you want to use special characters?")
 
     passwordPlaceHolder.push(pwSpecial);
 
+    // if no move out of array to arrayHolder
+    if (pwSpecial == true) {
+      arrayHolder.splice(0, 0, pwOptions[3]);
+      // console.log(arrayHolder);
+    }
     console.log(passwordPlaceHolder);
   }
 
   // validate input checks for length range
   makeArray();
+  
   if(passwordPlaceHolder[0] < 8 || passwordPlaceHolder[0] > 128){
+    // console.log(passwordPlaceHolder);
+    return "Password length must be no shoter than 8 characters and no longer than 128 characters";
 
-    passwordPlaceHolder = [];
-    console.log(passwordPlaceHolder);
-    makeArray();
-
+  }else if(arrayHolder.length == 0){
+      return "You must select a character type";
   }else{
-
+      // randomizes which array to pull from
+      console.log(arrayHolder.length);
       for (var i = 0; i < passwordPlaceHolder[0]; i++) {
-        temp = Math.floor(Math.random() * pwOptions.length + 1);
+        temp = Math.floor(Math.random() * arrayHolder.length + 1);
         passwordTemp.push(temp);
         
       }
-      console.log(passwordTemp);
+      // console.log(passwordTemp);
       passwordTemp.forEach(number => {
         var hold;
-
+        
         if (number == 1) {
-          hold = lowerCase[Math.floor(Math.random() * lowerCase.length)];
-          
+          hold = arrayHolder[0][Math.floor(Math.random() * arrayHolder[0].length)]; 
         }else if (number == 2) {
-          hold = upperCase[Math.floor(Math.random() * upperCase.length)];
+          hold = arrayHolder[1][Math.floor(Math.random() * arrayHolder[1].length)];
         }else if (number == 3){
-          hold = numbers[Math.floor(Math.random() * numbers.length)];
-        }else{
-          hold = symbols[Math.floor(Math.random() * symbols.length)];
+          hold = arrayHolder[2][Math.floor(Math.random() * arrayHolder[2].length)];
+        }else if (number == 4){
+          hold = arrayHolder[3][Math.floor(Math.random() * arrayHolder[3].length)];
         }
-        password.push(hold);
-      });
+        
+        password.push(hold);  
+      });     
   }
-  console.log(password);
-  // display PW
-  passwordDisplay = "";
-  password.forEach(element => {
-    passwordDisplay += element;
-  });
-  return passwordDisplay;
+
+  // creates a string from the array for display
+   var passwordDisplay = password;
+
+  // clears array inorder to be reuseable
+  password = [];
+
+  return passwordDisplay.join("");
 }
 // Write password to the #password input
 function writePassword() {
